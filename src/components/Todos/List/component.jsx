aeadd116e6e3c262840b1f024b38f99007b8dc83
Masha <masha.sladky@gmail.com>
2023-02-28 19:31:21 +0200

    now todos is shown in table

--- src/components/Todos/List/component.jsx
+++ src/components/Todos/List/component.jsx
@@ -1,20 +1,59 @@
-import List from '@mui/material/List';
+import Checkbox from '@mui/material/Checkbox';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-import { Item } from "./Item/component";
 
-export const TodosList = ({todos, onRemoveTodo, onCheckTodo}) => (
-    <List>
-        {todos.map(({id, titleValue, descriptionValue, checked}) =>(
-            <Item
-            key={id}
-            id={id}
-            titleValue={titleValue}
-            descriptionValue={descriptionValue}
-            onRemoveTodo={onRemoveTodo}
-            onCheckTodo={onCheckTodo}
-            checked={checked}
-          />
+export const TodosList = ({todos, onRemoveTodo, onCheckTodo}) => {
+    const createData = (checked, id, title, description, remove) => {
+        return { 
+             checked:<Checkbox checked={checked}  onClick={() => onCheckTodo(id)} /> ,
+             id, 
+             title, 
+             description, 
+             remove:  <ListItemIcon>
+                    <IconButton aria-label="delete" onClick={() => onRemoveTodo(id)}>
+                        <DeleteIcon />
+                    </IconButton>
+                </ListItemIcon>}
+    }
 
-        ) )}
-    </List>
-)
+    const rows = todos.map(({id, titleValue, descriptionValue, checked}) => createData(checked, id, titleValue, descriptionValue));
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Status</TableCell>
+            <TableCell align="right">Id</TableCell>
+            <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Description</TableCell>
+            <TableCell align="right">Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">{row.checked}</TableCell>
+              <TableCell align="right">{rows.indexOf(row) + 1}</TableCell>
+              <TableCell align="right">{row.title}</TableCell>
+              <TableCell align="right">{row.description}</TableCell>  
+              <TableCell align="right">{row.remove}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
